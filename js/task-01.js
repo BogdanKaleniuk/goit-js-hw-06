2265ed833b60bb195cb8c639b4c7227b4db7afc0
BogdanKaleniuk <bogdankaleniuk95@gmail.com>
2022-07-20 14:00:07 +0300

    done 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,11 @@
 const totalCagegories = document.querySelectorAll('.item');
+console.log('Number of categories:', totalCagegories.length);
+
+const categoriesArray = [...totalCagegories]
+  .map(
+    categories =>
+      `Category: ${categories.children[0].textContent}, Elements: ${categories.children[1].children.length}`
+  )
+  .join('\n');
+
+console.log(categoriesArray);
