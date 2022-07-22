c8070f988a49b6bdfdb9e06281be1ed9688fc50d
BogdanKaleniuk <bogdankaleniuk95@gmail.com>
2022-07-22 16:37:17 +0300

    done 2 task

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,27 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+let ulList = document.getElementById('ingredients');
+const liForEach = ingredients.forEach(ingredient => {
+  let elemLi = document.createElement('li');
+  elemLi.classList.add('list');
+  elemLi.innerHTML = ingredient;
+  // ulList.append(elemLi);
+  // ulList.append(...elemLi);
+  let ulGo = document.querySelector('ul');
+  ulGo.append(elemLi);;
+});
+console.log(liForEach);
+
+// elemLi.classList.add('item');
+// elemLi.textContent = [...ingredients];
+// document.body.ingredients.append(elemLi);
+// console.log(elemLi);
+// let ulList = document.getElementById('ingredients');
+// const foodIngredients = ingredients.forEach(ingredient => {
+//   let items = document.createElement('li');
+//   items.innerHTML = ingredient;
+//   ulList.append(items);
+//   parent.append(...items);
+// });
+
+// console.log(foodIngredients);
