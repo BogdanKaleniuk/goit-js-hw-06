ec302b5d8ea1d7a108c40adc1f1e8370b70702ac
BogdanKaleniuk <bogdankaleniuk95@gmail.com>
2022-07-25 12:28:08 +0300

    done 9

--- js/task-09.js
+++ js/task-09.js
@@ -12,7 +12,39 @@
 //     .padStart(6, 0)}`;
 // }
 
+const body = document.querySelector('body');
+const btnchangeColor = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+
+btnchangeColor.addEventListener('click', changeColor);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function changeColor() {
+  // console.log(changeColor);
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = body.style.backgroundColor;
+}
+// console.log(document.body.style.backg
+
+// --- Решение 2 ---
+
+// const refs = {
+//   body: document.body,
+//   btnchangeColor: document.querySelector('.change-color'),
+//   color: document.querySelector('.color'),
+// };
+// refs.btnchangeColor.addEventListener('click', changeColor);
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// function changeColor() {
+//   // console.log(changeColor);
+//   refs.body.style.backgroundColor = getRandomHexColor();
+//   refs.color.textContent = refs.body.style.backgroundColor;
+// }
+// // console.log(document.body.style.backgroundColor);
